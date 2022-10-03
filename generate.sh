fa726eabc1ebdf42a27fdf4d2721b5086c468239
Torge Rosendahl <torge.rosendahl@gmail.com>
2022-10-03 13:03:44 -0400

    fixed command generation

--- generate.sh
+++ generate.sh
@@ -9,7 +9,7 @@ commentpattern="\s*#.*"
 # group 1: cron time settings
 # group 3: filename
 # group 5: time interval for dir (can be empty)
-filterpattern="\s*(([^\s]+\s+){5})([^\s]+)\s*(\s+(\d+)){0,1}"
+filterpattern="\s*(([^\s]+\s+){5})([^\s]+)\s*(\s+(\d+)){0,1}$"
 
 #TODO remove old file from crontab
 #TODO delete old generated file
@@ -18,23 +18,29 @@ while read line; do
     if [[ $line =~ $commentpattern ]]; then
         continue
     fi
-    # get filename from line
+
     [[ $line =~ $filterpattern ]]
+    # get time command
+    timecmd="${BASH_REMATCH[1]}"
+    # get filename from line
     filename="${BASH_REMATCH[3]}"
 
     if [ -d $filename ]; then
+        # filename is directory
         echo "adding dir  \"$filename\""
         interval="${BASH_REMATCH[5]}"
         if [ "$interval" = "" ]; then
             interval="1800"
         fi
-        command="python3 ksetwallpaper/ksetwallpaper.py -d $filename -t $interval"
+        command="${timecmd}python3 ksetwallpaper/ksetwallpaper.py -d $filename -t $interval"
         echo "-> $command"
     elif [ -f $filename ]; then
+        # filename is single file
         echo "adding file \"$filename\""
-        command="python3 ksetwallpaper/ksetwallpaper.py --file $filename"
+        command="${timecmd}python3 ksetwallpaper/ksetwallpaper.py --file $filename"
         echo "-> $command"
     else
+        # filename does not exist
         echo "file \"$filename\" not found!"
         exit 1
     fi
